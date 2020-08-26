4928f06cc5555de36c584ed4dfcd31acb396d239
abdullayevahilal <HilalMa@code.edu.az>
2020-08-26 11:38:10 +0400

    admin panel added and product errors fixed

--- TravelProject/Controllers/CategoriesController.cs
+++ TravelProject/Controllers/CategoriesController.cs
@@ -25,29 +25,7 @@ namespace Travel.Controllers
             _productRepository = productRepository;
         }
 
-        public IActionResult Index(int id /*/*CategorySearchViewModel categorySearch*/)
-        {
-            var category = _categoryRepository.GetCategoryById(/*categorySearch.*/id);
-
-            if (category == null) return NotFound();
-
-            var products = _productRepository.GetProductsByCategoryId(category.Id);
-                                                                      //categorySearch.Limit,
-                                                                      //(categorySearch.Page - 1) * categorySearch.Limit,
-                                                                      //Repository.Enums.ProdcutListing.Newness);
-            //var productCount = _productRepository.GetProductCountByCategoryId(category.Id);
-
-            var model = new CategoryListViewModel
-            {
-                Category = _mapper.Map<Category, CategoryViewModel>(category),
-                Products = _mapper.Map<IEnumerable<Product>, IEnumerable<ProductViewModel>>(products)
-                //Count = productCount,
-                //Page = categorySearch.Page,
-                //Limit = categorySearch.Limit
-            };
-
-            return View(model);
-        }
+      
     }
     //public IActionResult Index()
     //    {
--- TravelProject/Startup.cs
+++ TravelProject/Startup.cs
@@ -40,8 +40,8 @@ namespace TravelProject
             services.AddTransient<IDepartmentRepository, DepartmentRepository>();
             services.AddTransient<IProductRepository, ProductRepository>();
             //services.AddTransient<ICategoryRepository, CategoryRepository>;
-          
-           
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
