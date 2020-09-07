b1124868d72fafe1ebbea5dbfe15dd4e9a15fabb
abdullayevahilal <HilalMa@code.edu.az>
2020-09-07 20:49:02 +0400

    navbar and product fixed

--- Admin/Controllers/DashboardController.cs
+++ Admin/Controllers/DashboardController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Admin.Controllers
 
 {
-    [TypeFilter(typeof(Auth))]
+    //[TypeFilter(typeof(Auth))]
     public class DashboardController : Controller
     {
         public IActionResult Index()
